27110b1da0d518c8253bec029e26298ddf459617
firasyazid <98762038+firasyazid@users.noreply.github.com>
2024-10-26 17:01:59 +0100

    26/10/24/2

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener} from '@angular/core';
 import {SharedService} from '../shared.service';
 
 @Component({
@@ -10,12 +10,26 @@ export class AdminComponent implements OnInit {
   title = 'EMSAT';
   navSidebarClass: boolean = true;
   hamburgerClass: boolean = false;
+  isDesktopView: boolean = true;
 
     constructor(public sharedService: SharedService) {
       
     }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+      this.checkScreenSize(); // Initial check
+    }
+  
+
+
+
+  @HostListener('window:resize', [])
+  onResize(): void {
+    this.checkScreenSize(); // Check on window resize
+  }
+
+  checkScreenSize(): void {
+    this.isDesktopView = window.innerWidth >= 768; // Set breakpoint for desktop view (e.g., 768px)
   }
 
 }
